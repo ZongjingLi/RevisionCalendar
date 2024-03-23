b157b380f592b3b4efe6e6385c270dbf72a14249
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2024-03-23 14:28:51 +0800

    add changes and add the main.change

--- app/src/main/java/com/example/revisioncalendar/MainActivity.kt
+++ app/src/main/java/com/example/revisioncalendar/MainActivity.kt
@@ -15,17 +15,8 @@ import com.example.revisioncalendar.ui.theme.RevisionCalendarTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            RevisionCalendarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.main);
+
     }
 }
 
