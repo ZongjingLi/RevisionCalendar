62d2b6c2cf7bc7910e6643acb1f7a70c0076891b
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2024-03-23 14:58:46 +0800

    add the activity page and modify the widgits and activity data type

---
+++ app/src/main/java/com/example/revisioncalendar/DataWrapper/Activity.java
@@ -0,0 +1,12 @@
+package com.example.revisioncalendar.DataWrapper;
+
+public class Activity {
+    private String title;
+    public Activity(String title) {
+        this.title = title;
+    }
+
+    public String toString() {
+        return this.title;
+    }
+}
--- app/src/main/java/com/example/revisioncalendar/MainActivity.kt
+++ app/src/main/java/com/example/revisioncalendar/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.revisioncalendar
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,15 +14,42 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.revisioncalendar.ui.theme.RevisionCalendarTheme
+import java.time.LocalDateTime;
+import com.example.revisioncalendar.DataWrapper.Activity;
 
 class MainActivity : ComponentActivity() {
+    var buttonToActivity: Button? = null;
+    var eventsData: ArrayList<Activity> = ArrayList<Activity>();
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main);
+        setupWigits();
+        setupData();
+        System.out.println("This is an actual real page.");
+        System.out.println(eventsData.get(0));
+    }
 
+    fun setupWigits() {
+        buttonToActivity = findViewById<View>(R.id.toSpecificButton) as Button
+        System.out.println("Try to setup the wigets.");
+    }
+
+    fun setupData() {
+        eventsData.add(Activity("Scourge"));
+    }
+
+    fun showSoftKeyboard(view: View) {
+        if (view.requestFocus()) {
+            val imm: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+        }
     }
 }
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
